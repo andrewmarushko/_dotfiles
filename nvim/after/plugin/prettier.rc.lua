2067e9797ae89b85ef8bdf46b7554c59904236f0
Andrew Marushko <>
2022-08-19 22:06:54 +0300

    Fix: listen project config on save prettier

--- nvim/after/plugin/prettier.rc.lua
+++ nvim/after/plugin/prettier.rc.lua
@@ -1,17 +1,26 @@
 local status, prettier = pcall(require, "prettier")
 if (not status) then return end
 
+
+vim.cmd [[
+  autocmd BufWritePre *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.md Prettier
+]]
+
 prettier.setup {
-  bin = 'prettierd',
+  bin = 'prettier',
   filetypes = {
     "css",
+    "graphql",
+    "html",
     "javascript",
     "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
     "typescript",
     "typescriptreact",
-    "json",
-    "scss",
-    "less",
-    "sass"
-  }
+    "yaml"
+  },
+  config_precedence = 'prefer-file'
 }
